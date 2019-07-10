33db1984c76f40e534bb985d1d011da18c119377
rverdile <ryanverdile@gmail.com>
2019-07-09 20:48:45 -0400

    Message bug fix

--- functions.py
+++ functions.py
@@ -66,7 +66,7 @@ def getAlertMessage(error_list):
 		error_list_final[i] = error_list[i]
 		
 	for i in range(4):
-		if(error_list_final[i]==1):
+		if(error_list_final[i]==1 or error_list_final[i]==2):
 			if(i==0):
 				#get error 1
 				with open("/home/pi/Alert_System/error_1.txt",'r') as file:
